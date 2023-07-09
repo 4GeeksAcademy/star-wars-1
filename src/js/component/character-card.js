8bebd001a5de2217aaa6516e123b72469845d14f
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-07-09 23:57:03 +0000

    fourth

--- src/js/component/character-card.js
+++ src/js/component/character-card.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom';
 export default function CharacterCard() {
 
     const [characters, setCharacters] = useState([])
@@ -8,21 +9,20 @@ export default function CharacterCard() {
             const result = await fetch("https://www.swapi.tech/api/people");
             const data = await result.json();
             setCharacters(data.results);
-            
         }
         fetchData()
-        
     }, [])
     console.log(characters)
     return (
-        <div>
+        <div className='d-flex col-10 overflow-auto mt-5 mx-auto'>
             {characters.length ? characters.map((character, index) => (
-                <div className="card" style={{width: "18rem"}} key={index}>
+                <div className="card" style={{width: "25rem"}} key={index}>
                     {console.log(character)}
                     <img src="https://www.sideshow.com/cdn-cgi/image/quality=90,f=auto/https://www.sideshow.com/storage/product-images/400369/the-child_star-wars_gallery_63f96869ef49c.jpg" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{character.name}</h5>
-                        <a href="#" className="btn btn-primary">Learn More</a>
+                        <Link to= {"/characterDescription/" + character.uid}
+                        className='btn btn-primary'>Learn More</Link>
                     </div>
                 </div>
             )) : null}
--- src/js/views/characterDescription.js
+++ src/js/views/characterDescription.js
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React,{useState, useEffect, useContext} from 'react'
+import { useParams } from 'react-router'
 export default function CharacterDescription() {
+  const[character,setCharacter]=useState({})
+  const{id}=useParams()
+  useEffect(() => {
+    async function fetchData() {
+        const result = await fetch("https://www.swapi.tech/api/people/"+id);
+        const data = await result.json();
+        setCharacter(data.results);
+    }
+    fetchData()
+}, [])
   return (
-    <div>characterDescription</div>
+    <div>
+      <h1>{character.name}</h1>
+      <h2>{character.hair_color}</h2>
+      <h2>{character.eye_color}</h2>
+      <h2>{character.birth_year}</h2>
+      <h2>{character.homeworld}</h2>
+      <h2>{character.description}</h2>
+
+    </div>
   )
 }
