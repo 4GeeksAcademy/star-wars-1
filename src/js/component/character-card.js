9ef0c4c732a97f2c545d0ea0604cb64b60b819a7
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-07-09 23:26:13 +0000

    second

--- src/js/component/character-card.js
+++ src/js/component/character-card.js
@@ -5,18 +5,20 @@ export default function CharacterCard() {
 
     useEffect(() => {
         async function fetchData() {
-            const result = await fetch("https://www.swapi.tech/api/people")
-            const data = result.json
-            setCharacters(data)
-            console.log(characters)
+            const result = await fetch("https://www.swapi.tech/api/people");
+            const data = await result.json();
+            setCharacters(data);
+            
         }
         fetchData()
+        console.log(characters)
     }, [])
     return (
         <div>
             {characters.length ? characters.map((character, index) => (
                 <div className="card" style={{width: "18rem"}} key={index}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    {console.log(character)}
+                    <img src="https://www.sideshow.com/cdn-cgi/image/quality=90,f=auto/https://www.sideshow.com/storage/product-images/400369/the-child_star-wars_gallery_63f96869ef49c.jpg" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{character.name}</h5>
                         <a href="#" className="btn btn-primary">Learn More</a>
